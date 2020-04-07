9e3f8e318bed8a2a7c5347ccc0eb2c26f0bc1791
CharlotteLAston <22291142@student.uwa.edu.au>
2020-04-07 11:11:58 +0800

    Updated plots and scripts with missing lengths included

--- 07_stacked bar plots.R
+++ 07_stacked bar plots.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 
 # Study name ----
-study <- "2020-01_Guardian-Ningaloo_stereoBRUVs" 
+study <- "ningaloo" 
 
 # Libraries required
 install_github("UWAMEGFisheries/GlobalArchive") #to check for updates
@@ -43,36 +43,36 @@ theme.larger.text<-theme(
   legend.title = element_text(family="TN",size=8),
   legend.text = element_text(family="TN",size=8))
 
-# Load fish pictures for plotting ----
-setwd(images.dir)
-dir()
-
-n.b <- readPNG("Nemipterus_bathybius_nb_GIBBONS.png")
-n.b <- as.raster(n.b)
-
-n.v <- readPNG("Nemipteridae.png")
-n.v <- as.raster(n.v)
-
-d.spp <- readPNG("Decapterus_spp.png")
-d.spp <- as.raster(d.spp)
-
-s.u <- readPNG("Synodus_variegatus_nb.png")
-s.u <- as.raster(s.u)
-
-c.p <- readPNG("Carcharinus plumbeus 5cmL_nb.png")
-c.p <- as.raster(c.p)
-
-l.l <- readPNG("Lagocephalus_lunaris_nb_BORNT.png")
-l.l <- as.raster(l.l)
-
-c.e <- readPNG("Carangoides_equula_nb_GIBBONS.png")
-c.e <- as.raster(c.e)
-
-d.c <- readPNG("Dentex_carpenteri_nb_GIBBONS.png")
-d.c <- as.raster(d.c)
-
-s.l <- readPNG("Sphyrna_lewini_nb_GIBBONS.png")
-s.l <- as.raster(s.l)
+# # Load fish pictures for plotting ----
+# setwd(images.dir)
+# dir()
+# 
+# n.b <- readPNG("Nemipterus_bathybius_nb_GIBBONS.png")
+# n.b <- as.raster(n.b)
+# 
+# n.v <- readPNG("Nemipteridae.png")
+# n.v <- as.raster(n.v)
+# 
+# d.spp <- readPNG("Decapterus_spp.png")
+# d.spp <- as.raster(d.spp)
+# 
+# s.u <- readPNG("Synodus_variegatus_nb.png")
+# s.u <- as.raster(s.u)
+# 
+# c.p <- readPNG("Carcharinus plumbeus 5cmL_nb.png")
+# c.p <- as.raster(c.p)
+# 
+# l.l <- readPNG("Lagocephalus_lunaris_nb_BORNT.png")
+# l.l <- as.raster(l.l)
+# 
+# c.e <- readPNG("Carangoides_equula_nb_GIBBONS.png")
+# c.e <- as.raster(c.e)
+# 
+# d.c <- readPNG("Dentex_carpenteri_nb_GIBBONS.png")
+# d.c <- as.raster(d.c)
+# 
+# s.l <- readPNG("Sphyrna_lewini_nb_GIBBONS.png")
+# s.l <- as.raster(s.l)
 
 # read in maxn
 setwd(tidy.dir)
@@ -87,7 +87,7 @@ maxn.sum<-maxn%>%
   group_by(scientific)%>%
   dplyr::summarise(maxn=sum(maxn))%>%
   ungroup()%>%
-  top_n(15)
+  top_n(10)
 
 ## Total frequency of occurance
 bar<-ggplot(maxn.sum, aes(x=reorder(scientific,maxn), y=maxn)) +   
@@ -108,12 +108,12 @@ maxn.10<-maxn.sum%>%
   filter(!scientific=="Carcharhinus sp1")
 
 ## Make labels for x axis
-allbarlabs<-c("Terapon jarbua","Carcharhinus sorrah","Saurida undosquamis","Netuma thalassina","Caranx ignobilis","Pristipomoides multidens","Decapterus spp","Lagocephalus lunaris","Nemipterus spp","Carangoides chrysophrys")
+allbarlabs<-c('Argyrops spinifer|Carangoides chrysophrys|Carangoides gymnostethus|Decapterus spp|Gymnocranius grandoculis|Lagocephalus sceleratus|Lethrinus miniatus|Lethrinus rubrioperculatus|Pristipomoides multidens|Pterocaesio digramma')
 
 ## Top ten plot ----
 bar.top.10<-ggplot(maxn.10, aes(x=reorder(scientific,maxn), y=maxn)) +   
   geom_bar(stat="identity",colour="black",fill="lightgrey",position=position_dodge())+
-  ylim (0, 250)+
+  ylim (0, 1000)+
   coord_flip()+
   xlab("Species")+
   ylab(expression(Overall~abundance~(Sigma~MaxN)))+
@@ -124,17 +124,17 @@ bar.top.10<-ggplot(maxn.10, aes(x=reorder(scientific,maxn), y=maxn)) +
   theme_bw()+
   theme(axis.text.y = element_text(face="italic"))+
   theme_collapse+
-  theme.larger.text+
-  annotation_raster(n.b, xmin=9.75,xmax=10.25,ymin=210, ymax=250)+
-  annotation_raster(d.c, xmin=8.6,xmax=9.4,ymin=175, ymax=214)+
-  annotation_raster(d.spp, xmin=7.6, xmax=8.5, ymin=50, ymax=108)+
-  annotation_raster(c.e, xmin=6.6,xmax=7.4,ymin=28, ymax=65)+
-  annotation_raster(n.v, xmin=5.7,xmax=6.3,ymin=15, ymax=50)+
-  annotation_raster(s.u, xmin=4.7,xmax=5.3,ymin=12, ymax=67)+
-  annotation_raster(c.p, xmin=3.4,xmax=4.7,ymin=9, ymax=105)+
-  annotation_raster(l.l, xmin=2.7,xmax=3.3,ymin=9, ymax=63)+
-  annotation_raster(s.l, xmin=1.55,xmax=2.5,ymin=6, ymax=95)+
-  annotation_raster(d.spp, xmin=0.7,xmax=1.3,ymin=5, ymax=50)
+  theme.larger.text
+  # annotation_raster(n.b, xmin=9.75,xmax=10.25,ymin=210, ymax=250)+
+  # annotation_raster(d.c, xmin=8.6,xmax=9.4,ymin=175, ymax=214)+
+  # annotation_raster(d.spp, xmin=7.6, xmax=8.5, ymin=50, ymax=108)+
+  # annotation_raster(c.e, xmin=6.6,xmax=7.4,ymin=28, ymax=65)+
+  # annotation_raster(n.v, xmin=5.7,xmax=6.3,ymin=15, ymax=50)+
+  # annotation_raster(s.u, xmin=4.7,xmax=5.3,ymin=12, ymax=67)+
+  # annotation_raster(c.p, xmin=3.4,xmax=4.7,ymin=9, ymax=105)+
+  # annotation_raster(l.l, xmin=2.7,xmax=3.3,ymin=9, ymax=63)+
+  # annotation_raster(s.l, xmin=1.55,xmax=2.5,ymin=6, ymax=95)+
+  # annotation_raster(d.spp, xmin=0.7,xmax=1.3,ymin=5, ymax=50)
 bar.top.10
 
 setwd(plots.dir)
