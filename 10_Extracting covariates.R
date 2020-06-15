752f3eb292150246cbba6595a88b98a202c37473
CharlotteLAston <22291142@student.uwa.edu.au>
2020-06-15 13:39:16 +0800

    Creating new covariates from bathymetry and extracting covariates for the sampled locations

---
+++ 10_Extracting covariates.R
@@ -0,0 +1,91 @@
+library(raster)
+library(rstudioapi)
+library(tidyverse)
+library(sf)
+library(dplyr)
+library(sp)
+library(rgdal)
+library(rgeos)
+library(spatialEco)
+
+##Set working directory----
+## Set work directory----
+working.dir <- dirname(rstudioapi::getActiveDocumentContext()$path) # sets working directory to where this script is saved (DON't MOVE THE SCRIPT)
+
+## Set sub directories----
+d.dir = paste(working.dir,"Tidy data",sep="/") 
+s.dir = paste(working.dir,"Spatial",sep="/") # spatial is where I keep spatial data files, rasters and shapefiles
+p.dir <- paste(working.dir,"Plots",sep="/")
+
+lat.long <- "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs"
+utm <- "+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+
+## Load bathymetry and tpi data----
+setwd(s.dir)
+bathy <- raster(paste(s.dir, "bathy.tif", sep='/'))
+bathy <- flip(bathy, direction="y")
+plot(bathy) # check it
+proj4string(bathy) # check the coordinate system, resolution, etc..
+
+
+tpi <- raster(paste(s.dir, "tpi.tif", sep='/'))
+plot(tpi) # check it
+proj4string(tpi) # check the coordinate system, resolution, etc..
+tpi <- flip(tpi, direction="y")
+plot(tpi)
+
+## Load BRUV data
+bruv <-  read.csv(paste(d.dir, "ningaloo.complete.maxn.csv", sep ='/'))
+str(bruv) # check df
+names(bruv) # check the names of the columns with lat and lon
+# Extract bits we need
+bruv <- bruv %>%
+  select("sample", "longitude", "latitude")
+bruv
+
+
+# Turn bruv data into spatial points --
+coordinates(bruv) <- ~ longitude + latitude
+points(bruv) # check it - this should plot the points on top of your previously plotted tpi or bathy
+proj4string(bruv) # check coordinate system and/or projection
+# If no projection give assign one,
+proj4string(bruv) <- lat.long
+# Convert to the same projection as the bathy/tpi
+# Rasters and spatial points should have the sample coordinate system and/or projection
+bruv <- spTransform(bruv, CRS("+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+points(bruv)
+
+## Create all covariates wanted - already have bathymetry and TPI
+
+Slope <- raster::terrain(bathy, opt="slope")
+Aspect <- raster::terrain(bathy, opt="aspect")
+TRI <- raster::terrain(bathy, opt="tri")
+Roughness <- raster::terrain(bathy, opt="roughness")
+FlowDir <- raster::terrain(bathy,opt="flowdir")
+
+## Extract perdictor data at bruv locations ----
+
+# Stack predictors --
+preds <- raster::stack(bathy,tpi,Slope,Aspect,TRI,Roughness,FlowDir) # if this fails: preds may have different extent or resolution
+
+# Extract predictors info --
+covariates <- raster::extract(preds, bruv, df = T) # this will result in dataframe with bruv points and predictor values
+
+# Give bruv uniques IDs
+bruv$ID <- covariates$ID
+
+# Fix column names
+names(covariates) <- c("ID", "bathymetry", "TPI", "Slope", "Aspect","TRI", "Roughness", "FlowDir")
+
+# Create full dataframe
+fullcovariates <- merge.data.frame(covariates, bruv, by="ID")
+
+# Save this data frame
+write.csv(fullcovariates, paste(d.dir, "covariates.csv", sep ='/')) 
+
+# Save stack of predictors if desired
+writeRaster(preds, paste(s.dir, "covariatestack.tif", sep ='/'))
+
+
+
