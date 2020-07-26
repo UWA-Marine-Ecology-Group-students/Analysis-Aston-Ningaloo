4801a869d7f8ceb30fd5305d594e3385c63f67fa
CharlotteLAston <22291142@student.uwa.edu.au>
2020-07-26 13:33:46 +0800

    Tidying up INLA script

--- a3_INLA.R
+++ a3_INLA.R
@@ -29,8 +29,6 @@ m.dir <- paste(working.dir,"Model Out INLA", sep="/")
 setwd(d.dir)
 data<- read.csv('final.data.csv')
 
-data.legal <- subset(data, model=='Legal')
-
 #remove NA sites 
 data<- data%>%
   filter(!sample%in%c("8.05","10.09","10.12","16.03"))%>%
@@ -44,91 +42,111 @@ covariates <- c("bathymetry","TPI","Slope","Aspect","FlowDir","mean.relief",
 factors <- c("status")
 covariates <- na.exclude(covariates)
 
+# Subet data
+data.legal <- subset(data, model=='Legal')
+data.sublegal <- subset(data, model=='Sublegal')
 
 ########## Setting up a mesh #########
 # This creates a mesh of discrete sampling locations that allows the model to estimate the spatial
 # autocorrelation in the data 
 
-# changing the mesh to reflect the coastline of WA
-# data <- data%>%
-#   rename(easting=latitude)%>%
-#   rename(northing=longitude)
-# 
-# Locations <- data%>%
-#   dplyr::select("easting", "northing")
-# 
-# coordinates(Locations) <- ~ northing + easting
-# 
-# setwd(s.dir)
-# 
-# interest <- readOGR(dsn = ".", layer = "Area of Interest")
-# coastline <- readOGR(dsn=".", layer = "Coastline")
-# 
-# plot(coastline)
-# plot(interest)
-# 
-# interest <- spTransform(interest, CRS("+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-# coastline <- spTransform(coastline, CRS("+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+# Rename coordinates
+data.legal <- data.legal%>%
+  rename(easting=latitude)%>%
+  rename(northing=longitude)
 
-# pll <- Polygon(interest@polygons[[1]]@Polygons[[1]]@coords, hole = F)
-# hole <- Polygon(coastline@polygons[[1]]@Polygons[[1]]@coords, hole = T)
-# new_sp <- SpatialPolygons(list(Polygons(append(hole, pll),'1')))
+Locations <- data.legal%>%
+  dplyr::select("easting", "northing")
 
-# plot(new_sp)
+# Turn into locations if using the modified coastline
+coordinates(Locations) <- ~ northing + easting
+
+setwd(s.dir)
+ 
+interest <- readOGR(dsn = ".", layer = "Area of Interest")
+coastline <- readOGR(dsn=".", layer = "Coastline")
+
+plot(coastline)
+plot(interest)
+ 
+interest <- spTransform(interest, CRS("+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+coastline <- spTransform(coastline, CRS("+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+pll <- Polygon(interest@polygons[[1]]@Polygons[[1]]@coords, hole = F)
+hole <- Polygon(coastline@polygons[[1]]@Polygons[[1]]@coords, hole = T)
+new_sp <- SpatialPolygons(list(Polygons(append(hole, pll),'1')))
+
+plot(new_sp)
 
 
 # Creating the mesh
-# my.mesh.land <- inla.mesh.2d(Locations, boundary=new_sp, max.edge = c(3500))
+my.mesh.land <- inla.mesh.2d(Locations, boundary=new_sp, max.edge = c(3500)) #This is the modified one
 
 my.mesh <- inla.mesh.2d(Locations, max.edge=c(6000))
 
-plot(my.mesh)
+plot(my.mesh.land)
 points(Locations, col = 2, pch = 16, cex = 0.2)
 
 ######### Setting the priors ########
 
 spat.priors <- inla.spde2.pcmatern(
-  mesh=my.mesh, alpha=2, ### alpha can usually be left at 2 unless using a 3d mesh
+  mesh=my.mesh.land, alpha=2, ### alpha can usually be left at 2 unless using a 3d mesh
   constr=TRUE,### so that random effects are constrained to sum to zero
-  prior.range=c(3000, 0.05), ### P(practic.range<3000)=0.05
-  #This is saying that the probability the range is less than 10000 is 5%
-  prior.sigma=c(5, 0.1)) ### P( sigma>5)=0.01  sigma is sd.
-  #This is saying that probability the variance is greater than 1 is less than 1%
+  prior.range=c(300, 0.05), ### 300 for legal, not sure for sub-legal
+  #This is saying that the probability the range is less than 300 is 5%
+  prior.sigma=c(0.5, 0.08)) ### 0.5,0.08 for legal, not sure for sub-legal
+  #This is saying that probability the variance is greater than 0.5 is less than 8%
 
 ######### Making the A matrix ########
 # This translates the spatial locations on the mesh to vectors in the model
-A.matrix <- inla.spde.make.A(my.mesh, loc = as.matrix(data[,c("easting","northing")]))
+A.matrix <- inla.spde.make.A(my.mesh.land, loc = as.matrix(data.legal[,c("easting","northing")]))
 
 ######## Creating a stack #######
 # This is a combination of the A. matrix and the mesh 
 
 mesh.index <- inla.spde.make.index(name = "iSpat", n.spde = spat.priors$n.spde)
-my.stack <- inla.stack(data=list(y=data$target.fish), A=list(A.matrix, 1),
+my.stack <- inla.stack(data=list(y=data.legal$target.fish), A=list(A.matrix, 1),
                        tag = "preds",
-                        effects=list(c(mesh.index, list( intercept=1)),#for the spatial effects and intercept
-                                      list(bathymetry=data$bathymetry, TPI=data$TPI, Slope=data$Slope, 
-                                           Aspect=data$Aspect, FlowDir=data$FlowDir,
-                                           mean.relief=data$mean.relief, sd.relief=data$sd.relief, 
-                                           reef=data$reef, distance.to.ramp=data$distance.to.ramp, 
-                                           status=data$status))) #covariates
+                       effects=list(c(mesh.index, list( intercept=1)),#for the spatial effects and intercept
+                                    list(bathymetry=data.legal$bathymetry, TPI=data.legal$TPI, Slope=data.legal$Slope, 
+                                         Aspect=data.legal$Aspect, FlowDir=data.legal$FlowDir,
+                                         mean.relief=data.legal$mean.relief, sd.relief=data.legal$sd.relief, 
+                                         reef=data.legal$reef, distance.to.ramp=data.legal$distance.to.ramp, 
+                                         status=data.legal$status))) #covariates
 
 ###### Run Full Model #########
 f.s <- y ~ -1 + intercept + bathymetry + TPI + Slope + Aspect + FlowDir + mean.relief + sd.relief + 
-  reef + distance.to.ramp + status + f(data$site, model='iid') + f(iSpat, model=spat.priors)
+  reef + distance.to.ramp + status + f(iSpat, model=spat.priors)
+# + f(data.legal$site, model="iid")
 
 fm <- inla(f.s,
-            family = "zeroinflatedpoisson1", #might use zeroinflated poisson as you've got a lot of zeros, link is the same
-            data = inla.stack.data(my.stack),
-            verbose=FALSE,
-            control.predictor=list(A=inla.stack.A(my.stack), compute=TRUE, link=1),
-            control.fixed = list(mean=0, prec=0.2),
-            control.results = list(return.marginals.random = TRUE, return.marginals.predictor = TRUE), 
-            control.compute=list(config = TRUE, dic=TRUE)
+           family = "zeroinflatedpoisson1",
+           data = inla.stack.data(my.stack),
+           verbose=FALSE,
+           control.predictor=list(A=inla.stack.A(my.stack), compute=TRUE, link=1),
+           control.fixed = list(mean=0, prec=0.2),
+           control.results = list(return.marginals.random = TRUE, return.marginals.predictor = TRUE), 
+           control.compute=list(config = TRUE, dic=TRUE)
 )
 
 summary(fm)
 
 plot(fm)
+
+####### Plotting the residuals and checking the model ######
+# If the model is well calibrated then the bins should be the same height. The convex model suggests that
+# the model is overconfident in it's predictions and the line graph suggests we've got both over and 
+# under fitting 
+ggplot_inla_residuals(fm, data.legal$target.fish, binwidth = 0.1)
+
+# This plots the values predeicted by the model against the actual values 
+index.pred <- inla.stack.index(my.stack, "preds")$data
+
+post.mean.pred <- fm$summary.fitted.values[index.pred, "mean"]
+post.sd.pred <- fm$summary.fitted.values[index.pred, "sd"]
+
+plot(post.mean.pred,data.legal$target.fish, ylim=c(0,13), xlim=c(0,12))
+
 
 ######## Plotting the results ###########
 #There are no p-values with INLA you look at significance by finding the overlap of the 0.025 and the 0.975
@@ -140,18 +158,18 @@ summary <- summary%>%
   rename(upper=`0.975quant`)%>%
   rename(estimate=mean)%>%
   mutate(factor=c("intercept", "bathy", "TPI", "slope", "aspect", "flowdir", "mean.relief", "sd.relief",
-           "reef", "ramp", "fished", "NTZ"))
+                  "reef", "ramp", "fished", "NTZ"))
 
 effects <- ggplot(summary,
-            aes(x = estimate,
-                y = factor, color=factor)) +
-            geom_point() +
-              geom_errorbar(aes(xmin=lower, xmax=upper), width=.5)+
-              geom_vline(xintercept = 0)+
+                  aes(x = estimate,
+                      y = factor, color=factor)) +
+  geom_point() +
+  geom_errorbar(aes(xmin=lower, xmax=upper), width=.5)+
+  geom_vline(xintercept = 0)+
   theme_bw()
 effects
 
-#So SD.reef, reef, ramp(?), flowdir, bathy look promising 
+#So reef, ramp(?), flowdir, bathy look promising 
 fm$summary.fixed
 
 ###### Projecting the posterior mean and SD of the spatial field########
@@ -183,109 +201,14 @@ gsd <- ggplot(df, aes(x = x, y = y, fill = sd_s)) +
 
 plot_grid(gmean, gsd)
 
-
-####### Plotting the residuals ######
-# If the model is well calibrated then the bins should be the same size. The convex model suggests that
-# the model is overconfident in it's predictions and the line graph suggests we've got both over and 
-# under fitting 
-ggplot_inla_residuals(fm, data$target.fish, binwidth = 0.1)
-
 ####### Stepwise Model selection ########
-bestmodel <- INLAstep(fam1="zeroinflatedpoisson1", data, spde = spat.priors,  in_stack=my.stack, 
-                  invariant = "-1+f(iSpat, model=spat.priors)", direction="backwards", 
-                  include=6:14, y='y', powerl = 1, inter=3)
+bestmodel <- INLAstep(fam1="zeroinflatedpoisson1", data.legal, spde = spat.priors,  in_stack=my.stack, 
+                      invariant = "-1+f(iSpat, model=spat.priors)", direction="backwards", 
+                      include=6:14, y='y', powerl = 1, inter=1)
 
 autoplot(bestmodel$best_model, which = c(1, 5), CI = TRUE)
 
 
-######### Predicting fish abundance using subset of data to test priors ########
-# Randomly selected 25% of the sites to be used as predictive sites rather than used in the model
-training <- data%>%
-  filter(!sample%in%c("19.03","10.11","13.03","14.03", "4.01", "1.06", "12.06", "10.13", "19.02",
-                      "4.12", "6.12", "30.02", "15.02", "30.01", "16.05", "3.06", "1.04", "16.02",
-                      "9.01", "23.06", "13.08", "2.02", "14.06", "6.03", "14.11", "6.02", "23.02",
-                      "14.12", "30.06", "5.08", "18.03", "18.07")) 
-predictive <- data%>%
-  filter(sample%in%c("19.03","10.11","13.03","14.03", "4.01", "1.06", "12.06", "10.13", "19.02",
-                      "4.12", "6.12", "30.02", "15.02", "30.01", "16.05", "3.06", "1.04", "16.02",
-                      "9.01", "23.06", "13.08", "2.02", "14.06", "6.03", "14.11", "6.02", "23.02",
-                      "14.12", "30.06", "5.08", "18.03", "18.07")) 
-
-# Set your covariates/spatial data
-covariates <- c("bathymetry","TPI","Slope","Aspect","FlowDir","mean.relief",
-                "sd.relief","reef","distance.to.ramp")
-factors <- c("status")
-covariates <- na.exclude(covariates)
-
-
-####### Make a mesh using the training data set ########
-Locations.train <- training%>%
-  dplyr::select("easting", "northing")
-
-pred.mesh <- inla.mesh.2d(Locations.train, max.edge=c(6000))
-
-plot(pred.mesh)
-points(Locations.train, col = 2, pch = 16, cex = 0.2)
-
-######### Setting the priors for prediction ########
-spat.priors.predict <- inla.spde2.pcmatern(
-  mesh=pred.mesh, alpha=2, ### alpha can usually be left at 2 unless using a 3d mesh
-  constr=TRUE,### so that random effects are constrained to sum to zero
-  prior.range=c(3000, 0.05), ### P(practic.range<3000)=0.05
-  #This is saying that the probability the range is less than 10000 is 5%
-  prior.sigma=c(5, 0.1)) ### P( sigma>5)=0.01  sigma is sd.
-#This is saying that probability the variance is greater than 1 is less than 1%
-
-######### Making the A matrix for prediction ########
-# Need one that has the data points and one that doesn't so we can predict over the area
-Pred.matrix <- inla.spde.make.A(pred.mesh, loc = as.matrix(training[,c("easting","northing")]))
-Pred.matrix.2 <- inla.spde.make.A(mesh = pred.mesh)
-
-######## Creating the stacks #######
-# Again need two stacks, one that has all the points in the training data and one that has NA for response
-
-mesh.index <- inla.spde.make.index(name = "iSpat", n.spde = spat.priors.predict$n.spde)
-
-stack.train <- inla.stack(data=list(y=training$target.fish), A=list(Pred.matrix, 1),
-                       tag = "train",
-                       effects=list(c(mesh.index, list( intercept=1)),#for the spatial effects and intercept
-                                    list(bathymetry=training$bathymetry, TPI=training$TPI, Slope=training$Slope, 
-                                         Aspect=training$Aspect, FlowDir=training$FlowDir,
-                                         mean.relief=training$mean.relief, sd.relief=training$sd.relief, 
-                                         reef=training$reef, distance.to.ramp=training$distance.to.ramp, 
-                                         status=training$status))) 
-
-stack.pred <- inla.stack(data=list(y=NA), A=list(Pred.matrix.2),
-                          tag = "preds",
-                          effects=list(c(mesh.index, list(intercept=1))))
-
-StackJoin <- inla.stack(stack.train, stack.pred)
-
-##### Run model ######
-
-f.s <- y ~ -1 + intercept + bathymetry + TPI + Slope + Aspect + FlowDir + mean.relief + sd.relief + 
-  reef + distance.to.ramp + status + f(iSpat, model=spat.priors)
-
-fm <- inla(f.s,
-           family = "zeroinflatedpoisson1", #might use zeroinflated poisson as you've got a lot of zeros, link is the same
-           data = inla.stack.data(StackJoin),
-           verbose=FALSE,
-           control.predictor=list(A=inla.stack.A(StackJoin), compute=TRUE, link=1),
-           control.fixed = list(mean=0, prec=0.2),
-           control.results = list(return.marginals.random = TRUE, return.marginals.predictor = TRUE), 
-           control.compute=list(config = TRUE, dic=TRUE))
-
-
-###### Selecting the posterior mean and SD of the response from the model #######
-index.pred <- inla.stack.index(StackJoin, "Pred")$data
-
-post.mean.pred <- fm$summary.linear.predictor[index.pred, "mean"]
-post.sd.pred <- fm$summary.linear.predictor[index.pred, "sd"]
-
-proj.grid <- inla.mesh.projector(pred.mesh, 
-                                 xlim = range(pred.grid[,1]), 
-                                 ylim = range(pred.grid[,2]), 
-                                 dims = c(ncol,nrow))
 
 
 
